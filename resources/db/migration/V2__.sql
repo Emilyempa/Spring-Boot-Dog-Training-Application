0f96485b08c68a3a54a316d4fe999449248553c4
Emilyempa <emilypettersson@hotmail.com>
2025-11-03 12:39:19 +0100

    Update initial migration script to add 'Dog' table, foreign key in 'DogTraining'

--- resources/db/migration/V2__.sql
+++ resources/db/migration/V2__.sql
@@ -1,22 +1,24 @@
-CREATE TABLE IF NOT EXISTS dog
+CREATE TABLE dog
 (
     id        INT AUTO_INCREMENT NOT NULL,
     name      VARCHAR(255)       NULL,
     breed     VARCHAR(255)       NULL,
-    birthdate DATE               NULL,
+    birthdate date               NULL,
     CONSTRAINT pk_dog PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS dog_training
+CREATE TABLE dog_training
 (
     id               INT AUTO_INCREMENT NOT NULL,
     activity         VARCHAR(255)       NULL,
     location         VARCHAR(255)       NULL,
-    training_date    DATE               NULL,
+    training_date    date               NULL,
     duration_minutes INT                NOT NULL,
     notes            LONGTEXT           NULL,
     dog_id           INT                NULL,
-    created_at       DATETIME           NULL,
-    CONSTRAINT pk_dogtraining PRIMARY KEY (id),
-    CONSTRAINT FK_DOGTRAINING_ON_DOG FOREIGN KEY (dog_id) REFERENCES dog (id)
-)
+    created_at       datetime           NULL,
+    CONSTRAINT pk_dogtraining PRIMARY KEY (id)
+);
+
+ALTER TABLE dog_training
+    ADD CONSTRAINT FK_DOGTRAINING_ON_DOG FOREIGN KEY (dog_id) REFERENCES dog (id);
