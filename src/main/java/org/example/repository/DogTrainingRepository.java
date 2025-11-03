40ac85757c173e215d24c061de047cbe33b9cb7b
Emilyempa <emilypettersson@hotmail.com>
2025-11-03 10:45:37 +0100

    Refactor from ListCrudRepository to JpaRepository

--- src/main/java/org/example/repository/DogTrainingRepository.java
+++ src/main/java/org/example/repository/DogTrainingRepository.java
@@ -1,7 +1,7 @@
 package org.example.repository;
 
 import org.example.entities.DogTraining;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DogTrainingRepository extends ListCrudRepository<DogTraining, Integer> { }
+public interface DogTrainingRepository extends JpaRepository<DogTraining, Integer> { }
 
